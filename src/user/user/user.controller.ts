4cb6dfa6ebac7d6585ffe45a4352e8659ba21359
afivve <afivve@gmail.com>
2024-11-14 09:21:54 +0700

    # HTTP

--- src/user/user/user.controller.ts
+++ src/user/user/user.controller.ts
@@ -1,4 +1,58 @@
-import { Controller } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Header,
+  HttpCode,
+  HttpRedirectResponse,
+  Param,
+  Post,
+  Query,
+  Redirect,
+  Req,
+} from '@nestjs/common';
+import { Request } from 'express';
 
 @Controller('/api/users')
-export class UserController {}
+export class UserController {
+  @Get('/sample-response')
+  @Header('Content-Type', 'application/json')
+  @HttpCode(200)
+  sampleResponse(): Record<string, string> {
+    return {
+      data: 'Hello JSON',
+    };
+  }
+
+  @Get('/redirect')
+  @Redirect()
+  redirect(): HttpRedirectResponse {
+    return {
+      url: '/api/users/sample-response',
+      statusCode: 301,
+    };
+  }
+
+  @Get('/hello')
+  sayHello(
+    @Query('first_name') firstName: string,
+    @Query('last_name') lastName: string,
+    @Query('age') age: number,
+  ): string {
+    return `Hello : ${firstName} ${lastName}, Umur : ${age}`;
+  }
+
+  @Get('/:id')
+  getById(@Param('id') id: string): string {
+    return `Params ID yang dikirimkan : ${id}`;
+  }
+
+  @Post()
+  post(): string {
+    return 'POST';
+  }
+
+  @Get('/sample')
+  get(): string {
+    return 'GET';
+  }
+}
